3605559a7dab976906fa70367557d3d365c64bbc
xKRISTOFx <gral1322@gmail.com>
2022-08-04 18:48:13 +0200

    54t

--- familiada.py
+++ familiada.py
@@ -13,6 +13,11 @@ while True:
     # Draw a red rectangle that resizes with the window.
     pygame.draw.rect(surface, (81,81,81), (stroke,stroke, surface.get_width()-stroke*2,surface.get_height()-stroke*2))
 
+    for i in range(1,11):
+        for j in range(1,30):
+            pygame.draw.rect(surface, (100,200,100), (stroke+i*(surface.get_width()-stroke*2)/10,stroke+j*(surface.get_height()-stroke*2)/30, (surface.get_width()-stroke*2)/10,(surface.get_height()-stroke*2)/30))
+
+
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
