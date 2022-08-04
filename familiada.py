3f3e6053564f4f759d6398d47c58fabfa5225130
radekaadek <radekaadek@gmail.com>
2022-08-04 20:38:53 +0200

    centered blocks

--- familiada.py
+++ familiada.py
@@ -22,16 +22,20 @@ while True:
     if surface.get_width() < surface.get_height()*(192/108):
         block_width = (surface.get_width()-125-(28*2))/29
         block_height = block_width*3/2
+        block_x = 0
+        block_y = (surface.get_height() - (block_height*10) - (9*2) - 100)/2
     else:
         block_height = (surface.get_height()-100-(9*2))/10
         block_width = block_height*2/3
+        block_x = (surface.get_width() - (block_width*29) - (28*2) - 125)/2
+        block_y = 0
 
     # Draw a red rectangle that resizes with the window.
     pygame.draw.rect(surface, (81,81,81), (stroke,stroke, surface.get_width()-stroke*2,surface.get_height()-stroke*2))
 
     for i in range(0,10):
         for j in range(0,29):
-            pygame.draw.rect(surface, (0,0,0), (50 +(block_width+3)*j, 50 + (block_height+3)*i, block_width, block_height))
+            pygame.draw.rect(surface, (0,0,0), (block_x + 50 +(block_width+3)*j, block_y + 50 + (block_height+3)*i, block_width, block_height))
 
 
     pygame.display.update()
