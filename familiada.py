a8c9b0e7db574407a5ef6c2f490175ff037e9dd5
xKRISTOFx <gral1322@gmail.com>
2022-08-04 18:19:55 +0200

    rectangle

--- familiada.py
+++ familiada.py
@@ -2,16 +2,16 @@ import pygame, sys
 
 pygame.init()
 # Create the window, saving it to a variable.
-surface = pygame.display.set_mode((350, 250), pygame.RESIZABLE)
+surface = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
 pygame.display.set_caption("Example resizable window")
 
+stroke = 20
+
 while True:
-    surface.fill((255,255,255))
+    surface.fill((0,0,255))
 
     # Draw a red rectangle that resizes with the window.
-    pygame.draw.rect(surface, (200,0,0), (surface.get_width()/3,
-      surface.get_height()/3, surface.get_width()/3,
-      surface.get_height()/3))
+    pygame.draw.rect(surface, (81,81,81), (stroke,stroke, surface.get_width()-stroke*2,surface.get_height()-stroke*2))
 
     pygame.display.update()
     for event in pygame.event.get():
@@ -25,5 +25,4 @@ while True:
 
         if event.type == pygame.VIDEORESIZE:
             # There's some code to add back window content here.
-            surface = pygame.display.set_mode((event.w, event.h),
-                                              pygame.RESIZABLE)
+            surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
