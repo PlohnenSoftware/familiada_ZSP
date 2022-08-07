9213a71563b8846a8a571a9226fc922a93ba4caa
xKRISTOFx <gral1322@gmail.com>
2022-08-07 22:33:19 +0200

    +

--- test2.py
+++ test2.py
@@ -16,12 +16,6 @@ nb.add(frame2, text="Tab2")
 
 def on_tab_change(event):
     tab = event.widget.tab("current")["text"]
-    if tab == "Tab1":
-        print("Tab1 is active")
-    elif tab == "Tab2":
-        print("Tab2 is active")
-
-
 nb.bind("<<NotebookTabChanged>>", on_tab_change)
 
 root.mainloop()
