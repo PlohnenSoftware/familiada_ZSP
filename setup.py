7ff008bf79aad666bad8c90bcad7fcfdf87a158f
radekaadek <radekaadek@gmail.com>
2024-10-22 19:26:03 +0200

    move paths

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ sys.argv.append("build_ext")
 sys.argv.append("--inplace")
 
 setup(
-    ext_modules=cythonize('helpers.pyx',  build_dir="build")
+    ext_modules=cythonize('src/helpers.pyx',  build_dir="build")
 )
 
 def find_name(name_regex: str) -> str | None:
@@ -26,5 +26,5 @@ if file_name is None:
     print("No file found with name helpers.c*")
     sys.exit(1)
 
-shutil.move(os.path.join(file_name), os.path.join("prec", os.path.basename(file_name)))
+shutil.move(os.path.join(file_name), os.path.join("src/prec", os.path.basename(file_name)))
 
